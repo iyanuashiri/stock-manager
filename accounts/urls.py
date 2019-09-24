d78acf4a019d2b253cb1eae5230b31c0d410bbcf
iyanuashiri <ajaoiyanu@gmail.com>
2019-09-24 23:14:39 +0100

    Add url router for AccountViewset

---
+++ accounts/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
+
+from . import viewsets
+
+
+schema_view = get_schema_view(title='Accounts API')
+
+router = DefaultRouter()
+router.register(r'accounts', viewsets.AccountViewset)
+
+
+app_name = 'accounts'
+urlpatterns = [
+    path('', include(router.urls)),
+    path('schema/', schema_view),
+
+]
