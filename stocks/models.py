3573abfaaaf1e243f98964c76192a05f2dcfc6fa
iyanuashiri <ajaoiyanu@gmail.com>
2019-09-25 02:01:32 +0100

    Add StockManager for buying stocks

--- stocks/models.py
+++ stocks/models.py
@@ -6,6 +6,12 @@ from accounts.models import Account
 # Create your models here.
 
 
+class StockManager(models.Manager):
+    def buy_stock(self, owner, name, symbol, unit_price, shares, total_price):
+        stock = self.create(owner=owner, name=name, symbol=symbol, unit_price=unit_price, shares=shares, total_price=total_price)
+        return stock
+
+
 class Stock(models.Model):
     owner = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='stocks')
     name = models.CharField(max_length=200)
@@ -13,6 +19,8 @@ class Stock(models.Model):
     unit_price = models.CharField()
     shares = models.IntegerField()
     total_price = models.CharField()
+
+    objects = StockManager()
 
     class Meta:
         verbose_name = 'stock'
