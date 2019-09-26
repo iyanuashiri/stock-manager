81cc5508aa2d76e3f696bd0ed6ca1a5b2e0b7721
iyanuashiri <ajaoiyanu@gmail.com>
2019-09-27 00:25:41 +0100

    Add name to url endpoints

--- stocks/urls.py
+++ stocks/urls.py
@@ -4,10 +4,10 @@ from stocks import views
 
 
 urlpatterns = [
-    path('stocks/', views.StockList.as_view()),
-    path('stocks/<symbol>/buy/<shares>/', views.StockBuy.as_view()),
-    path('stocks/<symbol>/sell/<shares>/', views.StockSell.as_view()),
-    path('stocks/<symbol>/search/', views.StockSearch.as_view()),
+    path('stocks/', views.StockList.as_view(), name='stock-list'),
+    path('stocks/<symbol>/buy/<shares>/', views.StockBuy.as_view(), name='stock-buy'),
+    path('stocks/<symbol>/sell/<shares>/', views.StockSell.as_view(), name='stock-sell'),
+    path('stocks/<symbol>/search/', views.StockSearch.as_view(), name='stock-search'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns=urlpatterns)
