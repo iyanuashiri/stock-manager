ad0e942e5cb82154d16b31b782ed009ade6ca44e
iyanuashiri <ajaoiyanu@gmail.com>
2019-09-27 21:53:00 +0100

    Add Testcase for StockBuy

--- stocks/tests/test_views.py
+++ stocks/tests/test_views.py
@@ -18,3 +18,12 @@ class TestStockList(APITestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    @pytest.mark.django_db
+    def test_stock_buy(self):
+        url = reverse('stock-buy', kwargs={'symbol': StockFactory.symbol, 'shares': StockFactory.shares})
+        account = AccountFactory()
+        stock = StockFactory(owner=account)
+        data = StockSerializer(stock)
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
