4c4a13ae942c63dc5f41223a8cf40907955be0a4
iyanuashiri <ajaoiyanu@gmail.com>
2019-09-27 15:28:21 +0100

    Add StockFactory

---
+++ stocks/factories.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+import factory
+
+from accounts.factories import AccountFactory
+
+from .models import Stock
+
+
+class StockFactory(factory.DjangoModelFactory):
+
+    class Meta:
+        model = Stock
+
+    owner = factory.SubFactory(AccountFactory)
+    name = factory.Faker('name')
+    symbol = factory.Faker('name')
+    unit_price = factory.Faker('number')
+    shares = factory.Faker('number')
+    total_price = factory.LazyAttribute(lambda a: f'{a.unit_price}*{a.shares}')
+    bought_date = factory.LazyAttribute(datetime.now())
