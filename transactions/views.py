4c37220bc394c10d54348818ba346d789141559d
iyanuashiri <ajaoiyanu@gmail.com>
2019-09-28 15:49:12 +0100

    Replace APIView with ListAPIView

--- transactions/views.py
+++ transactions/views.py
@@ -2,7 +2,7 @@ import datetime
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics
 
 from .models import Transaction
 from .serializers import TransactionSerializer
@@ -10,12 +10,14 @@ from .serializers import TransactionSerializer
 # Create your views here.
 
 
-class TransactionList(APIView):
+class TransactionList(generics.ListAPIView):
     """Lists all transactions by a user or list transactions between a period of time
 
     :returns: list of transaction objects.
     :rtype: JSON
     """
+    serializer_class = TransactionSerializer
+
     def get(self, request, *args, **kwargs):
         start_date = request.query_params.get('start_date', None)
         end_date = request.query_params.get('end_date', None)
@@ -23,5 +25,5 @@ class TransactionList(APIView):
             actions = Transaction.objects.filter(user=self.request.user, created__range=(Transaction.parse_date(start_date), Transaction.parse_date(end_date)))
         else:
             actions = Transaction.objects.filter(user=self.request.user)
-        serializer = TransactionSerializer(actions)
+        serializer = self.serializer_class(actions)
         return Response(serializer.data, status=status.HTTP_200_OK)
