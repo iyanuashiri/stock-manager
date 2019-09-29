9a786a4e90623b1bc43ad85025e0a554f12eb5aa
iyanuashiri <ajaoiyanu@gmail.com>
2019-09-29 19:06:31 +0100

    Use GenericRelatedField in TransactionSerializer

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -1,12 +1,18 @@
 from rest_framework import serializers
+from generic_relations.relations import GenericRelatedField
 
+from stocks.models import Stock
+from stocks.serializers import StockSerializer
 from .models import Transaction
 
 
 class TransactionSerializer(serializers.HyperlinkedModelSerializer):
+    target = GenericRelatedField({
+        Stock: StockSerializer(),
+    })
     url = serializers.HyperlinkedRelatedField(read_only=True, view_name='transactions:transaction-detail')
     user = serializers.HyperlinkedRelatedField(read_only=True, view_name='accounts:account-detail')
 
     class Meta:
         model = Transaction
-        fields = ('id', 'url', 'user', 'verb', 'created', 'target_ct', 'target_id', 'target')
+        fields = ('id', 'url', 'user', 'verb', 'created',  'target')
