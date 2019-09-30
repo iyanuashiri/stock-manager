03f184bdf161d452450688bcb5d1132775928ba4
iyanuashiri <ajaoiyanu@gmail.com>
2019-09-30 05:38:48 +0100

    Fixes

--- stocks/utils.py
+++ stocks/utils.py
@@ -6,7 +6,8 @@ IEX_TOKEN = config('IEX_TOKEN')
 
 
 def search_stock(symbol):
-    stock = Stock(symbol, token=IEX_TOKEN)
-    price = stock.get_price()
+    stock = Stock(symbol, token='sk_c8589e5108e741ad8d4c98a2b003bf3b')
     company_name = stock.get_company_name()
+    quote = stock.get_quote()
+    price = quote['latestPrice']
     return price, company_name
