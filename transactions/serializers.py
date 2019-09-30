38ac579079dfd8df897e4dad3dd8470225c0a9c3
iyanuashiri <ajaoiyanu@gmail.com>
2019-09-30 08:34:23 +0100

    Fixes

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -6,13 +6,12 @@ from stocks.serializers import StockSerializer
 from .models import Transaction
 
 
-class TransactionSerializer(serializers.HyperlinkedModelSerializer):
+class TransactionSerializer(serializers.ModelSerializer):
     target = GenericRelatedField({
         Stock: StockSerializer(),
     })
-    url = serializers.HyperlinkedRelatedField(read_only=True, view_name='transactions:transaction-detail')
     user = serializers.HyperlinkedRelatedField(read_only=True, view_name='accounts:account-detail')
 
     class Meta:
         model = Transaction
-        fields = ('id', 'url', 'user', 'verb', 'created',  'target')
+        fields = ('id', 'user', 'verb', 'created', 'target')
--- transactions/views.py
+++ transactions/views.py
@@ -10,7 +10,7 @@ from .serializers import TransactionSerializer
 # Create your views here.
 
 
-class TransactionList(generics.ListAPIView):
+class TransactionList(APIView):
     """Lists all transactions by a user or list transactions between a period of time
 
     :returns: list of transaction objects.
@@ -19,12 +19,12 @@ class TransactionList(generics.ListAPIView):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
 
-    def get_queryset(self):
-        start_date = self.request.query_params.get('start_date', None)
-        end_date = self.request.query_params.get('end_date', None)
+    def get(self, request, *args, **kwargs):
+        start_date = request.query_params.get('start_date', None)
+        end_date = request.query_params.get('end_date', None)
         if start_date and end_date is not None:
             transactions = self.queryset.filter(user=self.request.user, created__range=(Transaction.parse_date(start_date), Transaction.parse_date(end_date)))
         else:
             transactions = self.queryset.filter(user=self.request.user)
-        serializer = TransactionSerializer(transactions)
-        return serializer
+        serializer = TransactionSerializer(transactions, many=True, context={'request': request})
+        return Response(serializer.data, status=status.HTTP_200_OK)
